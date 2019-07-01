0028baae7f988f5b0261af07da855d46df650f2b
busradogann <busradogan1993@gmail.com>
2019-07-01 13:29:42 +0300

    refactor(home-page): package.json refactored.

--- src/Core/Button/SearchButton.jsx
+++ src/Core/Button/SearchButton.jsx
@@ -1,22 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 import "./_search-button.scss";
 
 export default class SearchButton extends Component {
+    static propTypes = {
+        query: PropTypes.string.isRequired,
+        selectedOption: PropTypes.string
+    }
+
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         return this.props.query !== nextProps.query || this.props.selectedOption !== nextProps.selectedOption;
     }
+
     render() {
-        const { query, selectedOption } = this.props;
+        const {
+            query,
+            selectedOption
+        } = this.props;
         let url = `/image-list?query=${query}`;
+
         if (selectedOption) {
             url = `${url}&collectionID=${selectedOption}`;
         }
+
         return (
             <Link to={url}
                   className={"search-button-context"}>
-                SEARCH
+                {"SEARCH"}
             </Link>
         )
     }
--- src/Core/Form/Form.jsx
+++ src/Core/Form/Form.jsx
@@ -9,23 +9,23 @@ import SearchButton from "../../Core/Button/SearchButton";
 const API_COLLECTIONS = "https://api.unsplash.com/collections/featured/?client_id=930640e0b7713dca3ab1a0751b6f4b4741d1dfca6a72be2a071cddd6c1d0c92c";
 
 export default class Form extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            query: '',
-            selectedOption: null,
-            collections: []
-        }
+    state = {
+        query: "",
+        selectedOption: null,
+        collections: []
     }
 
     changeHandler = (value) => {
-        this.setState({query : value})
-    };
+        this.setState({
+            query : value
+        });
+    }
 
     changeCollection = (value) => {
-        this.setState({selectedOption : value})
-    };
+        this.setState({
+            selectedOption : value
+        });
+    }
 
     componentDidMount = () => {
         let url = `${API_COLLECTIONS}&per_page=10`;
@@ -33,19 +33,26 @@ export default class Form extends Component {
         fetch(url)
             .then(response => response.json())
             .then(data => this.setState({ collections : data }));
-    };
+    }
 
     render() {
-        const { query, collections, selectedOption } = this.state;
+        const {
+            query,
+            collections,
+            selectedOption
+        } = this.state;
+
         return(
             <div className={"form-wrapper"}>
                 <InputLocation onChange={this.changeHandler}
-                               value={query} />
+                               value={query}/>
+
                 <CustomDropdown onChange={this.changeCollection}
                                 optionArray={collections}
-                                selectedOption={selectedOption} />
+                                selectedOption={selectedOption}/>
+
                 <SearchButton query={query}
-                              selectedOption={selectedOption} />
+                              selectedOption={selectedOption}/>
             </div>
         );
     }
